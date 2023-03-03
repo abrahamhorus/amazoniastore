3df2fec5882d7c7b2d90470aa96105222b163844
abrahamhorus <110262126+abrahamhorus@users.noreply.github.com>
2023-03-03 01:42:41 -0600

    se creo el primer producto

---
+++ src/components/Product.js
@@ -0,0 +1,117 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { AddShoppingCart} from '@mui/icons-material';
+
+
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+export default function Product() {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            R
+          </Avatar>
+        }
+        action={
+          <Typography
+          className='numero'
+          variant='h5'
+          color='textSecondary'
+          >
+           {"$1200 MXN"}
+          </Typography>
+        }
+        title="Tenis Naik"
+        subheader="disponible"
+      />
+      <CardMedia
+        component="img"
+        height="194"
+        image= 'https://http2.mlstatic.com/D_NQ_NP_902739-MLM51796124177_102022-O.webp'
+        alt="tenis naik"
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+        Tenis Deportivos.
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to cart" >
+          <AddShoppingCart />
+        </IconButton>
+        <IconButton >
+          {Array(4)
+          .fill()
+          .map((_, i) => (<p>&#11088;</p>))
+          }
+        </IconButton>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Method:</Typography>
+          <Typography paragraph>
+            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
+            aside for 10 minutes.
+          </Typography>
+          <Typography paragraph>
+            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
+            medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
+            occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
+            large plate and set aside, leaving chicken and chorizo in the pan. Add
+            pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
+            stirring often until thickened and fragrant, about 10 minutes. Add
+            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+          </Typography>
+          <Typography paragraph>
+            Add rice and stir very gently to distribute. Top with artichokes and
+            peppers, and cook without stirring, until most of the liquid is absorbed,
+            15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
+            mussels, tucking them down into the rice, and cook again without
+            stirring, until mussels have opened and rice is just tender, 5 to 7
+            minutes more. (Discard any mussels that don&apos;t open.)
+          </Typography>
+          <Typography>
+            Set aside off of the heat to let rest for 10 minutes, and then serve.
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
---
+++ src/product-data.js
@@ -0,0 +1,11 @@
+const products =[{
+id:1,
+name:"Tenis",
+productType: "Tenis Deportivos",
+price: 1200,
+rating: 4,
+image: 'https://http2.mlstatic.com/D_NQ_NP_902739-MLM51796124177_102022-O.webp',
+description: "Acaba tu última repetición con potencia y ruge hasta aturdir todo el piso del gimnasio con el Nike Air Max Alpha Trainer 5. La amortiguación Max Air ofrece comodidad y estabilidad para realizar levantamientos, ya sea una rutina ligera o más pesada. La base amplia y plana te brinda estabilidad y agarre para mejorar todo tipo de entrenamientos difíciles sin sacrificar el estilo, mientras recorres cada estación y te preparas para el ajuste."
+
+
+}]
